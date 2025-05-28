4d661c1653c5cc367f0cd02d4f98a3bacc41c737
Naman <namanmishra3116@gmail.com>
2025-05-28 12:40:36 +0530

    setup the appwrite

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,38 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+import Entypo from "@expo/vector-icons/Entypo";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Tabs } from "expo-router";
+const TabsLayout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "green" }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => {
+            return focused ? (
+              <FontAwesome size={20} color={color} name="home" />
+            ) : (
+              <Entypo size={20} color={color} name="home" />
+            );
+          },
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="login"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Login",
+          tabBarIcon: ({ color, focused }) => {
+            return focused ? (
+              <MaterialCommunityIcons size={20} color={color} name="login" />
+            ) : (
+              <Entypo size={20} color={color} name="login" />
+            );
+          },
         }}
       />
     </Tabs>
   );
-}
+};
+
+export default TabsLayout;
---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+const Login: React.FC = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello from login screen</Text>
+    </View>
+  );
+};
+
+export default Login;
